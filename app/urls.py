f43a075e776cb3ce693e6691fbdfcca208b62206
Rhuan Carlos <rhuancarlos.queiroz@gmail.com>
2019-10-02 12:02:09 -0300

    Removed unused imports

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import ListCreateWordView
 from .views import ListCreateLetterView
 from .views import RetrieveUpdateDestroyLetterView
