6987a630f73f0ffa6f98011e3792fa91847532df
Rhuan Carlos <rhuancarlos.queiroz@gmail.com>
2019-10-03 12:06:22 -0300

    Used multiple decorators

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.dispatch import receiver
-import os
 
 
 # Create your models here
@@ -27,6 +26,7 @@ class Letter(models.Model):
 
 
 @receiver(models.signals.post_delete, sender=Word)
+@receiver(models.signals.post_delete, sender=Letter)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
     """
     Deletes file from filesystem
@@ -36,17 +36,8 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
         instance.image.delete(False)
 
 
-@receiver(models.signals.post_delete, sender=Letter)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    """
-    Deletes file from filesystem
-    when corresponding Letter object is deleted.
-    """
-    if instance.image:
-        instance.image.delete(False)
-
-
 @receiver(models.signals.pre_save, sender=Word)
+@receiver(models.signals.pre_save, sender=Letter)
 def auto_delete_file_on_change(sender, instance, **kwargs):
     """
     Deletes old file from filesystem
@@ -59,26 +50,6 @@ def auto_delete_file_on_change(sender, instance, **kwargs):
     try:
         old_file = Word.objects.get(pk=instance.pk).image
     except Word.DoesNotExist:
-        return False
-
-    new_file = instance.image
-    if not old_file == new_file:
-        old_file.delete(False)
-
-
-@receiver(models.signals.pre_save, sender=Letter)
-def auto_delete_file_on_change(sender, instance, **kwargs):
-    """
-    Deletes old file from filesystem
-    when corresponding Letter object is updated
-    with new file.
-    """
-    if not instance.pk:
-        return False
-
-    try:
-        old_file = Letter.objects.get(pk=instance.pk).image
-    except Letter.DoesNotExist:
         return False
 
     new_file = instance.image
