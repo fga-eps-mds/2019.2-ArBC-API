740e743a880bf46b43ea37a44e669769daf5aa4a
Luciano Santos <luciano_z7@hotmail.com>
2019-10-04 12:07:43 -0300

    Added container registry test.

--- compose/local/django/start.sh
+++ compose/local/django/start.sh
@@ -6,5 +6,8 @@ set -o nounset
 set -o xtrace
 
 
+docker build -t registry.gitlab.com/lucianosz7/2019-2-arbc-api .
+docker push registry.gitlab.com/lucianosz7/2019-2-arbc-api
+
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
