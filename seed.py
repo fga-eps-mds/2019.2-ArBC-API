4bc84eefe670f621bcf208767b3ffad07ff136b0
marcelog5 <marcelovictorg2@gmail.com>
2019-10-04 00:01:09 -0300

    Linting corrections

--- seed.py
+++ seed.py
@@ -1,31 +1,32 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','api.settings')
-
 import django
-django.setup()
-
-from app.models import Letter
-from django.core.files.uploadedfile import InMemoryUploadedFile, \
-    SimpleUploadedFile
-from django.core.files.base import ContentFile
 import string
+import app
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.files.base import ContentFile
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+django.setup()
 
 
 def load_image(char):
-    content_file = ContentFile(open('seeds/' + char + '.gif','rb').read())
+
+    content_file = ContentFile(open('seeds/' + char + '.gif', 'rb').read())
     file_name = char
     image_file = InMemoryUploadedFile(content_file, None,
-                                        file_name + '.gif',
-                                        'image/gif', content_file.tell, None)
+                                      file_name + '.gif', 'image/gif',
+                                      content_file.tell, None)
 
     return image_file
 
+
 def creat_Letter(N):
-    letter = Letter()
+    letter = app.models.Letter()
     letter.name = N
     image = load_image(N)
     letter.image.save(image.name, image)
     letter.save()
+
 
 for i in string.ascii_uppercase:
     creat_Letter(i)
